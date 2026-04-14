88f201ca858e025c1a43251a574015b73c818be6
petreatheo <s4008914@student.rmit.edu.au>
2026-04-14 16:56:38 +1000

    Did the code for creatLoginTable, still need to connect AWS

--- database/createLoginTable.js
+++ database/createLoginTable.js
@@ -1,1 +1,34 @@
-TESTING
+const AWS = require('aws-sdk');
+
+// Configure region
+AWS.config.update({ region: 'us-east-1' });
+
+// Create DynamoDB service object
+const dynamodb = new AWS.DynamoDB();
+
+// define table structure
+const params = {
+    TableName: "login",
+
+    // define primary key
+    KeySchema: [
+        { AttributeName: "email", KeyType: "HASH" } // Partition key
+    ],
+
+    // define attribute types
+    AttributeDefinitions: [
+        { AttributeName: "email", AttributeType: "S" }
+    ],
+
+    // simple pricing model
+    BillingMode: "PAY_PER_REQUEST"
+};
+
+// creating table
+dynamodb.createTable(params, (err, data) => {
+    if (err) {
+        console.error("Error creating table:", err);
+    } else {
+        console.log("Table created successfully:", data);
+    }
+});
