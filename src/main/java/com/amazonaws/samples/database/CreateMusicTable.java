d77b7e8afb21a2228e054191c9a33cef5c34d720
petreatheo <s4008914@student.rmit.edu.au>
2026-04-21 16:42:01 +1000

    added note in website file

---
+++ src/main/java/com/amazonaws/samples/database/CreateMusicTable.java
@@ -0,0 +1,29 @@
+package com.amazonaws.samples.database;
+
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+
+public class CreateMusicTable {
+
+    public static void main(String[] args) {
+
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withRegion(Regions.US_EAST_1)
+                .withCredentials(new ProfileCredentialsProvider("default"))
+                .build();
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        String tableName = "music";
+
+        try {
+            System.out.println("Attempting to create table...");
+
+            CreateTableRequest request = new CreateTableRequest()
+                    .withTableName(tableName)
+                    .withKeySchema(
+}
