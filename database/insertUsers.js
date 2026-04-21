0fe68f802be5731d11d2cd34cc699dba072f2d24
petreatheo <s4008914@student.rmit.edu.au>
2026-04-21 15:26:04 +1000

    Changed the layout should be good to go!

--- database/insertUsers.js
+++
@@ -1,9 +0,0 @@
-const AWS = require('aws-sdk');
-
-// Configure region
-AWS.config.update({ region: 'us-east-1' });
-
-const docClient = new AWS.DynamoDB.DocumentClient();
-
-
-// will delete later adding this to commit and push
