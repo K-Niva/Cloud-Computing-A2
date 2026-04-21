b6f24620b83589ad28190bbaa7fe14d1a356277a
petreatheo <s4008914@student.rmit.edu.au>
2026-04-21 17:44:54 +1000

    started to createBuckets to upload images

---
+++ src/main/java/com/amazonaws/samples/database/CreateBucket.java
@@ -0,0 +1,41 @@
+package com.amazonaws.samples.database;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CreateBucketRequest;
+import com.amazonaws.services.s3.model.GetBucketLocationRequest;
+
+import java.io.IOException;
+
+
+public class CreateBucket {
+    public static void main(String[] args) {
+
+        Regions clientRegion = Regions.US_EAST_1;
+        String bucketName = "music-images";
+
+        try {
+            AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                    .withCredentials(new ProfileCredentialsProvider("default"))
+                    .withRegion(clientRegion)
+                    .build();
+
+            if (!s3Client.doesBucketExistV2(bucketName)) {
+
+                s3Client.createBucket(bucketName);
+
+                String location = s3Client.getBucketLocation(bucketName);
+                System.out.println("Bucket created. Location: " + location);
+            } else {
+                System.out.println("Bucket already exists.");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/amazonaws/samples/database/UploadImagesToS3.java
@@ -0,0 +1,15 @@
+package com.amazonaws.samples.database;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+import java.io.File;
+import java.io.IOException;
+
+public class UploadImagesToS3 {
+}
